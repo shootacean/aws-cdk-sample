946a59dd73a54050b54988f43cad3e06dfabc2ed
Shootacean <shootacean@gmail.com>
2019-11-30 14:38:30 +0900

    [add] vpc

--- lib/aws-cdk-sample-stack.ts
+++ lib/aws-cdk-sample-stack.ts
@@ -1,9 +1,12 @@
 import cdk = require('@aws-cdk/core');
+import ec2 = require('@aws-cdk/aws-ec2');
 
 export class AwsCdkSampleStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'Vpc', {
+      cidr: '10.0.0.0/16',
+    });
   }
 }
